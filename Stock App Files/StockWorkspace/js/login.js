49d193744e169faaaae5ea6ad677e45f7d90772b
Daniel Gaisberger <daniel.gaisberger@gmail.com>
2018-01-19 16:04:21 -0500

    DG - implemented the login using firebase - no error messages
    using the firebase login function

--- Stock App Files/StockWorkspace/js/login.js
+++ Stock App Files/StockWorkspace/js/login.js
@@ -1,20 +1,64 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBWRqFtzDSiyZfDD8WjvdERLo-NuraC8Bg",
-    authDomain: "stockapp-57f25.firebaseapp.com",
-    databaseURL: "https://stockapp-57f25.firebaseio.com",
-    projectId: "stockapp-57f25",
-    storageBucket: "stockapp-57f25.appspot.com",
-    messagingSenderId: "496167713307"
-};
-firebase.initializeApp(config);
+"use strict";
 
-var ref = firebase.database().ref("Users")
-console.log(ref);
-/*ref.orderByKey().startAt(document.getElementById('exampleInputEmail1').innerHTML).endAt(document.getElementById('exampleInputEmail1')).on("child_added", function(snapshot) {
-    console.log(snapshot.key);
-});*/
+//Logout a current user
+firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    console.log("Sign-out successful.")
+}).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode);
+    console.log(errorMessage);
+    // ...
+});
 
-ref.orderByKey().on("child_added", function(snapshot) {
-    console.log("User: "+snapshot.val().confirmPassword);
-})
+// Listen for form submit
+document.getElementById('loginForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+    e.preventDefault();
+
+    //Get values
+    var email = getInputVal('email');
+    var password = getInputVal('password');
+
+    //login user
+    loginUser(email, password);
+
+    //try if user is logged in
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+
+            //On success forward to index.html
+            window.location.replace("index.html");
+        } else {
+            // No user is signed in.
+        }
+    });
+
+}
+
+//Function to get form values
+function getInputVal(id){
+    //returns value of id
+    return document.getElementById(id).value;
+}
+
+// login a user
+// returns a promsie
+function loginUser(email, password) {
+    var deferred = $.Deferred();
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+    });
+
+    return deferred.promise();
+}
