d95d1f88fb5b48998d8e4ef48247e733b469ba38
Jarrett Horton <jhorton@eagles.bridgewater.edu>
2018-02-04 10:59:28 -0500

    JH - Autocomplete WIth minor bug

--- Stock App Files/StockWorkspace/js/SearchForStock.js
+++ Stock App Files/StockWorkspace/js/SearchForStock.js
@@ -1,3 +1,41 @@
+var autoURL = "https://api.iextrading.com/1.0/ref-data/symbols";
+
+// Creating function outside to later pass to the search for stock
+function AutoCompleteResult(value) {
+    // Showing value on the click
+    console.log(value);
+}
+
+// Auto complete for search for stock
+$.ajax({
+    url: autoURL,
+    success: function (data) {
+        var options = {
+            url: autoURL,
+            getValue: "name",
+            template: {
+                type: "custom",
+                method: function(value, item) {
+                    return value + "  (" + item.symbol + ")";
+                }
+            },
+            list: {
+                match: {
+                    enabled: true
+                },
+                onClickEvent: function() {
+                    var value = $("#myInput").getSelectedItemData().symbol;
+                    AutoCompleteResult(value);
+                    console.log(value);
+                }
+            }
+        };
+        $("#myInput").easyAutocomplete(options);
+    }
+});
+
+
+
 // Takes the input on button press and parses the string.
 function UrlStringPass() {
     var input = document.getElementById("myInput").value;
