227873abe2d45b5e75a7ee3b98c5b34e86480368
Unknown <isilvious@eagles.bridgewater.edu>
2018-02-14 20:59:23 -0500

    IS - filled in watchlist and porfolio tables in dashboard

--- Stock App Files/StockWorkspace/js/index.js
+++ Stock App Files/StockWorkspace/js/index.js
@@ -37,11 +37,29 @@ function getFullPortfolio(){
                     });
                 }
             }).then(function(){
-//work with data in here ... fill donut and so on
+                //work with data in here ... fill donut and so on
                 //array looks like this:
                 //[[userId, stockSymbol, date, price, quantity],[userId, stockSymbol, date, price, quantity],...]
-
                 console.log(fullPortfolio);
+
+                //populate portfolioTable
+                var table = document.getElementById("portfolioTable");
+                for(var i=0; i<fullPortfolio.length;i++){
+                    var stockTransferURL = "IndividualStockPage.html?stock=" + fullPortfolio[i].stockSymbol + "#";
+                    var row = table.insertRow(i+1);
+
+                    var cell0 = row.insertCell((0));
+                    cell0.innerHTML = fullPortfolio[i].stockSymbol.link(stockTransferURL);
+
+                    var cell1 = row.insertCell((1));
+                    cell1.innerHTML = fullPortfolio[i].date;
+
+                    var cell2 = row.insertCell((2));
+                    cell2.innerHTML = "$" + fullPortfolio[i].price;
+
+                    var cell3 = row.insertCell((3));
+                    cell3.innerHTML = fullPortfolio[i].quantity;
+                }
             });
         } else {
             // No user is signed in.
@@ -71,11 +89,20 @@ function getFullWatchlist(){
                     });
                 }
             }).then(function(){
-//work with data in here ... fill donut and so on
+                //work with data in here ... fill donut and so on
                 //array looks like this:
                 //[[userId, stockSymbol],[userId, stockSymbol],...]
-
                 console.log(fullWatchlist);
+
+                //populate portfolioTable
+                var table = document.getElementById("watchlistTable");
+                for(var i=0; i<fullWatchlist.length;i++){
+                    var stockTransferURL = "IndividualStockPage.html?stock=" + fullWatchlist[i].stockSymbol + "#";
+                    var row = table.insertRow(i+1);
+
+                    var cell0 = row.insertCell((0));
+                    cell0.innerHTML = fullWatchlist[i].stockSymbol.link(stockTransferURL);
+                }
             });
         } else {
             // No user is signed in.
