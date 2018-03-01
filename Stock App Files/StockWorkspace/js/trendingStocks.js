70d6ba72545a011a3919f18f784838b7f83acf0f
Unknown <isilvious@eagles.bridgewater.edu>
2018-02-28 20:47:23 -0500

    IS- trending stocks

--- Stock App Files/StockWorkspace/js/trendingStocks.js
+++ Stock App Files/StockWorkspace/js/trendingStocks.js
@@ -1,7 +1,11 @@
 var url = "https://cors-anywhere.herokuapp.com/" + "http://money.cnn.com/";
 $.get(url, function(response) {
-    //var cnnTable = response.replace(/<strong class="e63d1fd7">FCX/g).match(/FCX/g).length;
-    //console.log(cnnTable);
-    document.body.innerHTML = response;
-});
+    var regex1 = /FCX/g
+    var array1;
+
+    while ((array1 = regex1.exec(response)) !== null) {
+        console.log('Found ' + array1[0] + '. Next starts at ' + regex1.lastIndex + '.');
+
+    }
+})
 
